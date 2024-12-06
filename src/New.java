8121b0135b8eb65561995eed8d4db996d9a5492b
AkinoF <147878497+AkinoF@users.noreply.github.com>
2024-12-06 13:29:04 +0300

    Создан класс Газета

--- src/New.java
+++ src/New.java
@@ -1,2 +1,29 @@
+// Определение класса Газета
 public class New {
+    // Поля класса
+    private String nazvanie; // Название газеты
+    private int tirazh;      // Тираж газеты
+    private int kolichestvoPolos; // Количество полос
+
+    // Конструктор для инициализации всех полей
+    public New(String nazvanie, int tirazh, int kolichestvoPolos) {
+        this.nazvanie = nazvanie;
+        this.tirazh = tirazh;
+        this.kolichestvoPolos = kolichestvoPolos;
+    }
+
+    // Метод для получения полной информации о газете
+    public String info() {
+        return "Газета: " + nazvanie + ", Тираж: " + tirazh + ", Количество полос: " + kolichestvoPolos;
+    }
+
+    // Метод для изменения названия газеты
+    public void changeName(String newNazvanie) {
+        this.nazvanie = newNazvanie;
+    }
+
+    // Метод для изменения количества полос
+    public void changeNumber(int newKolichestvoPolos) {
+        this.kolichestvoPolos = newKolichestvoPolos;
+    }
 }
--- src/Newspaper.java
+++ src/Newspaper.java
@@ -1,2 +1,21 @@
+// Главный класс для тестирования
 public class Newspaper {
+    public static void main(String[] args) {
+        // Создание объектов класса Gazeta
+        New gazeta1 = new New("Новости Сегодня", 50000, 20);
+        New gazeta2 = new New("Время", 30000, 30);
+        New gazeta3 = new New("Наука и Жизнь", 15000, 15);
+
+        // Получение полной информации обо всех объектах
+        System.out.println(gazeta1.info());
+        System.out.println(gazeta2.info());
+        System.out.println(gazeta3.info());
+
+        // Изменение названия и количества полос одного объекта
+        gazeta1.changeName("Сегодняшние Новости");
+        gazeta1.changeNumber(25);
+
+        // Получение полной информации об измененном объекте
+        System.out.println(gazeta1.info());
+    }
 }
